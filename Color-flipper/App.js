e25177a162201a73c9651891b3f6fbf69128bfbc
jagtapshubham <jagtapshubham190@gmail.com>
2023-03-14 00:26:37 +0530

    modified App.js

--- Color-flipper/App.js
+++ Color-flipper/App.js
@@ -6,7 +6,12 @@ console.log(color);
 
 btn.addEventListener("click", function () {
     //get random number between 0 - 3
-    const randomNumber = 1;
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+    return Math.floor(Math.random()*colors.length);
+}

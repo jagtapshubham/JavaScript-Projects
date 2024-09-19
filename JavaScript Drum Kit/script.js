01d6feb9112150e5990ac081f7ee5d67c87a34e5
jagtapshubham <jagtapshubham190@gmail.com>
2024-09-19 18:57:51 +0530

    Drum Kit Music - javascript30

---
+++ JavaScript Drum Kit/script.js
@@ -0,0 +1,17 @@
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  key.classList.add("playing");
+  audio.currentTime = 0; // rewind to start
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
